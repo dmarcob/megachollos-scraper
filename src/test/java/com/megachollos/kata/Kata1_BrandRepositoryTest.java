7abad1c1c13487de783cb3f6c5447ad459cff808
MUMP <miguelmonpi@gmail.com>
2026-04-07 16:46:33 +0200

    Kata2

--- src/test/java/com/megachollos/kata/Kata1_BrandRepositoryTest.java
+++ src/test/java/com/megachollos/kata/Kata1_BrandRepositoryTest.java
@@ -1,23 +1,42 @@
 package com.megachollos.kata;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.megachollos.brand.infrastructure.jpa.entities.BrandEntity;
 import com.megachollos.brand.infrastructure.jpa.repositories.JpaBrandRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 /**
  * This test is BROKEN on purpose.
  * Your mission: make it work with a real PostgreSQL using Testcontainers.
  * Follow the instructions in kata-1-why-integration-tests.md
  */
+
 @DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class Kata1_BrandRepositoryTest {
+
+  @Container
+  @ServiceConnection
+  static PostgreSQLContainer<?> postgres =
+          new PostgreSQLContainer<>("postgres:17-alpine");
 
   @Autowired
   private JpaBrandRepository jpaBrandRepository;
+
+  @Autowired
+  private EntityManager entityManager;
 
   @Test
   void shouldSaveAndFindBrand() {
@@ -34,5 +53,23 @@ class Kata1_BrandRepositoryTest {
     var result = jpaBrandRepository.findById("samsung");
     assertThat(result).isPresent();
     assertThat(result.get().getDisplayName()).isEqualTo("Samsung");
+  }
+
+  @Test
+  void shouldFailWhenDuplicateUniqueName() {
+    BrandEntity brand1 = BrandEntity.builder()
+            .uniqueName("Samsung")
+            .displayName("1")
+            .build();
+
+    BrandEntity brand2 = BrandEntity.builder()
+            .uniqueName("Samsung")
+            .displayName("2")
+            .build();
+
+    jpaBrandRepository.saveAndFlush(brand1);
+    entityManager.detach(brand1);
+    assertThatThrownBy(() -> jpaBrandRepository.saveAndFlush(brand2))
+      .isInstanceOf(DataIntegrityViolationException.class);
   }
 }
